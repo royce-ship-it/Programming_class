1b613970e211465328387262c0fd858fd6ec9c3c
royce-ship-it <royce@baobabonlineacademy.com>
2026-09-04 21:01:02 +0100

    functions and stuff

--- python/fatipati.py
+++ python/fatipati.py
@@ -1,3 +1,18 @@
 #python file
-print(2*2)
+
+
+#+-*/
+#function that performs arithmatic operations
+def osz(kum, kay):
+    print(f"{kum * kay}")
+    print(f"{kum / kay}")
+    print(f"{kum - kay}")
+    print(f"{kum ** kay}")
+
+osz(5, 8)
+osz(6, -7)
+osz(13, 8)
+osz(-2, -9)
+
+
 
